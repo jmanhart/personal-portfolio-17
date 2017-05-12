39fd00407e71cf2c0a56cc6529bfea5ea48c0d26
Manhart, John <manhartjohn@gmail.com>
2017-05-11 19:59:24 -0500

    adding in some cooooool stuff

---
+++ src/containers/subPages/data/books.js
@@ -0,0 +1,21 @@
+export const booksData = [
+    {
+      "title": "Catcher in the Rye",
+      "author": "JD.Salinger",
+      "image": "../../src/public/images/books/catcher-in-the-rye.jpg",
+      "link":"https://www.amazon.com/Catcher-Rye-J-D-Salinger/dp/0316769487/ref=sr_1_1?ie=UTF8&qid=1494548313&sr=8-1&keywords=catcher+in+the+rye",
+    },
+    {
+      "title": "Ishmael",
+      "author":"Daniel Quinn",
+      "image":"",
+      "link":"",
+    },
+    {
+      "title": "Ishmael",
+      "author":"",
+      "image":"",
+      "link":"",
+    },
+
+]
--- src/containers/subPages/sub-01.js
+++ src/containers/subPages/sub-01.js
@@ -11,25 +11,47 @@ import './../../main.scss';
 //Import Specific Styles
 import './styles.scss'
 
+//Import Data
+import './data/books.js'
+
 const data = [
-    {"title": "Catcher in the Rye"},
-    {"title": "Ham on Rye"},
-    {"title": "God is not Great"},
-    {"title": "Hitch 22"},
-    {"title": "The Martian"},
-    {"title": "Under the Volcano"},
-    {"title": "Catcher in the Rye"},
-    {"title": "Ham on Rye"},
-    {"title": "God is not Great"},
-    {"title": "Hitch 22"},
-    {"title": "The Martian"},
-    {"title": "Under the Volcano"},
-    {"title": "Catcher in the Rye"},
-    {"title": "Ham on Rye"},
-    {"title": "God is not Great"},
-    {"title": "Hitch 22"},
-    {"title": "The Martian"},
-    {"title": "Under the Volcano"},
+    {
+      "title": "Catcher in the Rye",
+      "author": "JD.Salinger",
+      "image": "../../src/public/images/books/catcher-in-the-rye.jpg",
+      "link":"https://www.amazon.com/Catcher-Rye-J-D-Salinger/dp/0316769487/ref=sr_1_1?ie=UTF8&qid=1494548313&sr=8-1&keywords=catcher+in+the+rye",
+    },
+    {
+      "title": "Ishmael",
+      "author":"Daniel Quinn",
+      "image":"../../src/public/images/books/ishmael.jpg",
+      "link":"https://www.amazon.com/Ishmael-Novel-Daniel-Quinn/dp/0553375407/ref=sr_1_1?ie=UTF8&qid=1494550245&sr=8-1&keywords=ishmael",
+    },
+    {
+      "title": "Invisible Man",
+      "author":"Ralph Ellison",
+      "image":"../../src/public/images/books/invisible-man.jpg",
+      "link":"https://www.amazon.com/Invisible-Man-Ralph-Ellison/dp/0679732764/ref=sr_1_1?ie=UTF8&qid=1494550152&sr=8-1&keywords=invisible+man",
+    },
+    {
+      "title": "Mortality",
+      "author":"Christopher Hitches",
+      "image":"../../src/public/images/books/mortality.jpg",
+      "link":"https://www.amazon.com/Invisible-Man-Ralph-Ellison/dp/0679732764/ref=sr_1_1?ie=UTF8&qid=1494550152&sr=8-1&keywords=invisible+man",
+    },
+    {
+      "title": "Under The Volcano",
+      "author":"Malcom Lowery",
+      "image":"../../src/public/images/books/under-the-volcano.jpg",
+      "link":"https://www.amazon.com/Invisible-Man-Ralph-Ellison/dp/0679732764/ref=sr_1_1?ie=UTF8&qid=1494550152&sr=8-1&keywords=invisible+man",
+    },
+    {
+      "title": "Blood Merdian",
+      "author":"Cormac McCarthy",
+      "image":"../../src/public/images/books/blood-meridian.jpg",
+      "link":"https://www.amazon.com/Invisible-Man-Ralph-Ellison/dp/0679732764/ref=sr_1_1?ie=UTF8&qid=1494550152&sr=8-1&keywords=invisible+man",
+    },
+
 ]
 
 export default class SubPageOne extends Component{
@@ -40,10 +62,15 @@ export default class SubPageOne extends Component{
               <div className="item-grid-wrapper">
                 {data.map((item) => {
                   return (
-                    <div className="item-wrapper">
-                      <div className="item-thumbnail"/>
-                      <h2 className="item-label" key={item.title}>{item.title}</h2>
-                    </div>
+                      <div className="item-wrapper">
+                        <a key={item.title} href={item.link}>
+                        <div className="item-thumbnail">
+                          <img key={item.title} src={item.image}/>
+                        </div>
+                        <h2 className="item-title" key={item.title}>{item.title}</h2>
+                        <h3 className="item-author" key={item.author}>{item.author}</h3>
+                        </a>
+                      </div>
                   )
                 })}
               </div>
