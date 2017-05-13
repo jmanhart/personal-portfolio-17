fb41d7b3d254d9111f4317e94c0f6508330fe1ea
Manhart, John <manhartjohn@gmail.com>
2017-05-13 08:56:56 -0500

    adding in dummy content

--- src/containers/marlin/index.js
+++ src/containers/marlin/index.js
@@ -8,14 +8,35 @@ import { Link } from 'react-router';
 import './../../styles/styles.css';
 import './../../main.scss';
 
+//Import Components
+import BackIcon from '../../components/backIcon'
+import HeroBlock from '../../components/heroBlock'
+
 
 export default class Marlin extends Component{
     render(){
         return(
-            <div className="details-hero-container">
-                <h1>Marlin</h1>
+          <div>
+            <BackIcon/>
+            <HeroBlock label="Marlin" />
+            <div className="details-body-container">
+              <div className="details-body-content">
+                <h1>Headling and other cool stuff you know oh yeah</h1>
+                <p>
+                  Chillwave you probably haven't heard of them hella tofu jianbing bushwick. Activated charcoal pok pok bushwick, wolf pork belly gochujang lyft locavore roof party ugh tousled banh mi tofu glossier. Post-ironic banh mi lyft, master cleanse neutra brunch VHS. Bitters meggings pok pok XOXO venmo. Tumeric yuccie hammock artisan tote bag, chambray mlkshk blog retro dreamcatcher raclette raw denim sriracha listicle. Pickled offal heirloom, seitan blue bottle fixie single-origin coffee trust fund whatever pork belly neutra prism. Four dollar toast hella distillery, vexillologist meggings microdosing poutine iPhone brunch marfa butcher XOXO 90's.
+                </p>
+                <p>
+                  Chillwave you probably haven't heard of them hella tofu jianbing bushwick. Activated charcoal pok pok bushwick, wolf pork belly gochujang lyft locavore roof party ugh tousled banh mi tofu glossier. Post-ironic banh mi lyft, master cleanse neutra brunch VHS. Bitters meggings pok pok XOXO venmo. Tumeric yuccie hammock artisan tote bag, chambray mlkshk blog retro dreamcatcher raclette raw denim sriracha listicle. Pickled offal heirloom, seitan blue bottle fixie single-origin coffee trust fund whatever pork belly neutra prism. Four dollar toast hella distillery, vexillologist meggings microdosing poutine iPhone brunch marfa butcher XOXO 90's.
+                </p>
+                <p>
+                  Chillwave you probably haven't heard of them hella tofu jianbing bushwick. Activated charcoal pok pok bushwick, wolf pork belly gochujang lyft locavore roof party ugh tousled banh mi tofu glossier. Post-ironic banh mi lyft, master cleanse neutra brunch VHS. Bitters meggings pok pok XOXO venmo. Tumeric yuccie hammock artisan tote bag, chambray mlkshk blog retro dreamcatcher raclette raw denim sriracha listicle. Pickled offal heirloom, seitan blue bottle fixie single-origin coffee trust fund whatever pork belly neutra prism. Four dollar toast hella distillery, vexillologist meggings microdosing poutine iPhone brunch marfa butcher XOXO 90's.
+                </p>
+                <p>
+                  Chillwave you probably haven't heard of them hella tofu jianbing bushwick. Activated charcoal pok pok bushwick, wolf pork belly gochujang lyft locavore roof party ugh tousled banh mi tofu glossier. Post-ironic banh mi lyft, master cleanse neutra brunch VHS. Bitters meggings pok pok XOXO venmo. Tumeric yuccie hammock artisan tote bag, chambray mlkshk blog retro dreamcatcher raclette raw denim sriracha listicle. Pickled offal heirloom, seitan blue bottle fixie single-origin coffee trust fund whatever pork belly neutra prism. Four dollar toast hella distillery, vexillologist meggings microdosing poutine iPhone brunch marfa butcher XOXO 90's.
+                </p>
+              </div>
             </div>
+          </div>
         )
     }
 }
-
--- src/containers/mosey/index.js
+++ src/containers/mosey/index.js
@@ -10,14 +10,32 @@ import './../../main.scss';
 
 //Import Components
 import BackIcon from '../../components/backIcon'
+import HeroBlock from '../../components/heroBlock'
 
 export default class Mosey extends Component{
     render(){
         return(
-            <div className="details-hero-container">
-              <BackIcon />
-              <h1>Mosey</h1>
+          <div>
+            <BackIcon/>
+            <HeroBlock label="Mosey" />
+            <div className="details-body-container">
+              <div className="details-body-content">
+                <h1>Headling and other cool stuff you know oh yeah</h1>
+                <p>
+                  Chillwave you probably haven't heard of them hella tofu jianbing bushwick. Activated charcoal pok pok bushwick, wolf pork belly gochujang lyft locavore roof party ugh tousled banh mi tofu glossier. Post-ironic banh mi lyft, master cleanse neutra brunch VHS. Bitters meggings pok pok XOXO venmo. Tumeric yuccie hammock artisan tote bag, chambray mlkshk blog retro dreamcatcher raclette raw denim sriracha listicle. Pickled offal heirloom, seitan blue bottle fixie single-origin coffee trust fund whatever pork belly neutra prism. Four dollar toast hella distillery, vexillologist meggings microdosing poutine iPhone brunch marfa butcher XOXO 90's.
+                </p>
+                <p>
+                  Chillwave you probably haven't heard of them hella tofu jianbing bushwick. Activated charcoal pok pok bushwick, wolf pork belly gochujang lyft locavore roof party ugh tousled banh mi tofu glossier. Post-ironic banh mi lyft, master cleanse neutra brunch VHS. Bitters meggings pok pok XOXO venmo. Tumeric yuccie hammock artisan tote bag, chambray mlkshk blog retro dreamcatcher raclette raw denim sriracha listicle. Pickled offal heirloom, seitan blue bottle fixie single-origin coffee trust fund whatever pork belly neutra prism. Four dollar toast hella distillery, vexillologist meggings microdosing poutine iPhone brunch marfa butcher XOXO 90's.
+                </p>
+                <p>
+                  Chillwave you probably haven't heard of them hella tofu jianbing bushwick. Activated charcoal pok pok bushwick, wolf pork belly gochujang lyft locavore roof party ugh tousled banh mi tofu glossier. Post-ironic banh mi lyft, master cleanse neutra brunch VHS. Bitters meggings pok pok XOXO venmo. Tumeric yuccie hammock artisan tote bag, chambray mlkshk blog retro dreamcatcher raclette raw denim sriracha listicle. Pickled offal heirloom, seitan blue bottle fixie single-origin coffee trust fund whatever pork belly neutra prism. Four dollar toast hella distillery, vexillologist meggings microdosing poutine iPhone brunch marfa butcher XOXO 90's.
+                </p>
+                <p>
+                  Chillwave you probably haven't heard of them hella tofu jianbing bushwick. Activated charcoal pok pok bushwick, wolf pork belly gochujang lyft locavore roof party ugh tousled banh mi tofu glossier. Post-ironic banh mi lyft, master cleanse neutra brunch VHS. Bitters meggings pok pok XOXO venmo. Tumeric yuccie hammock artisan tote bag, chambray mlkshk blog retro dreamcatcher raclette raw denim sriracha listicle. Pickled offal heirloom, seitan blue bottle fixie single-origin coffee trust fund whatever pork belly neutra prism. Four dollar toast hella distillery, vexillologist meggings microdosing poutine iPhone brunch marfa butcher XOXO 90's.
+                </p>
+              </div>
             </div>
+          </div>
         )
     }
 }
