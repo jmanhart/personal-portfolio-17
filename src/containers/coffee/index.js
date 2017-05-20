f56fd1cf9d95f77ae11a9a8c235cbad0f955677a
Manhart, John <manhartjohn@gmail.com>
2017-05-20 10:13:46 -0500

    Coffee grid

--- src/containers/coffee/index.js
+++ src/containers/coffee/index.js
@@ -8,18 +8,55 @@ import { Link } from 'react-router';
 import './../../styles/styles.css';
 import './../../main.scss';
 
+import './styles.scss'
+
 //Import Components
 import BackIcon from '../../components/backIcon'
 import HeroBlock from '../../components/heroBlock'
-import CardGrid from '../../components/cardGrid'
 
+
+const data = [
+    {
+      "city": "Austin",
+      "state":"Texas",
+    },
+    {
+      "city": "Kansas City",
+      "state":"MO",
+    },
+    {
+      "city": "Springfield",
+      "state":"MO",
+    },
+    {
+      "city": "St. Louis",
+      "state":"MO",
+    },
+    {
+      "city": "Tulsa",
+      "state":"Oklahoma",
+    },
+]
 
 export default class Coffee extends Component{
     render(){
         return(
           <div>
             <BackIcon/>
-            <CardGrid />
+              <div className="card-grid-container">
+                <div className="card-grid">
+                  {data.map((item) => {
+                    return (
+                      <a className="card" key={item.city} href={item.link}>
+                        <div className="card-content">
+                          <span className="card-city" key={item.city} >{item.city}</span>
+                          <span className="card-state" key={item.state} >{item.state}</span>
+                        </div>
+                      </a>
+                    )
+                  })}
+                </div>
+              </div>
           </div>
         )
     }
