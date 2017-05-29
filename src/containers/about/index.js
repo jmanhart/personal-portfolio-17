36f197664f2b1bec128290a4127bcefda9dd006b
Manhart, John <manhartjohn@gmail.com>
2017-05-29 14:57:21 -0500

    Work push adding past cities

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -10,17 +10,30 @@ import './styles.scss'
 export default class About extends Component{
     render(){
         return(
+          <div>
             <div className="about-wrapper">
               <div className="about-block">
-
                 <span className="label-01">👋 I'm John.</span>
                 <span className="label-02">A Designer, Developer currently living in Kansas City</span>
                 <span className="label-04">Currently at Garmin</span>
               </div>
               <div>
-                <span className="label-05">More Shit Below</span>
+                <span className="label-05">More Shit About Me Below</span>
               </div>
             </div>
+
+            <div className="about-wrapper">
+              <div className="about-block">
+                <span className="label-01"><s>ARKANSAS</s></span>
+                <span className="label-01"><s>MEXICO</s></span>
+                <span className="label-01"><s>MICHIGAN</s></span>
+                <span className="label-01"><s>MISSOURI</s></span>
+                <span className="label-01">KANSAS CITY</span>
+              </div>
+            </div>
+
+          </div>
+
         )
     }
 }
