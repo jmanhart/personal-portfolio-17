8c4cbdd8bbad447204286210009df2396968c73d
Manhart, John <manhartjohn@gmail.com>
2018-02-17 17:48:38 -0600

    purging some code

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -24,64 +24,10 @@ export default class Home extends Component{
     render(){
         return(
           <div id="page">
-
-            {/*<header className="side-navigation-container">
-                <div className="site-links">
-
-                  <div className="branding">
-                    <span className="label-03">✌</span>
-                    <span className="label-03">John</span>
-                    <span className="label-03">Manhart</span>
-                    <span className="label-04">User Experince</span>
-                  </div>
-
-                  <div className="internal-links">
-                    <ul>
-                      <Link activeStyle={ACTIVE} to="/about">
-                        <li><span className="internal-nav-label">About</span></li>
-                      </Link>
-                      <Link activeStyle={ACTIVE} to="/work">
-                        <li><span className="internal-nav-label">Work</span></li>
-                      </Link>
-                      <Link activeStyle={ACTIVE} to="/marlin">
-                        <li><span className="internal-nav-label">Blog</span></li>
-                      </Link>
-                      <Link activeStyle={ACTIVE} to="/favorites">
-                        <li><span className="internal-nav-label">Likes</span></li>
-                      </Link>
-                      <Link activeStyle={ACTIVE} to="/coffee">
-                        <li><span className="internal-nav-label">Coffee</span></li>
-                      </Link>
-                    </ul>
-                  </div>
-
-                  <div className="external-links">
-                    <i className="fa fa-dribbble fa-1x"/>
-                    <i className="fa fa-github fa-1x"/>
-                    <i className="fa fa-instagram fa-1x"/>
-                  </div>
-
-                  </div>
-            </header>
-
-            <div className="details-wrapper">
-              <div className="about-block">
-                <span className="label-01 strike">Arkansas</span>
-                <span className="label-01 strike">Mexico</span>
-                <span className="label-01 strike">Michigan</span>
-                <span className="label-01 strike">Missouri</span>
-                <span className="label-01">Kansas City</span>
-              </div>
-            </div>
-
-            */}
-
             <div className="main-content">
               {this.props.children}
             </div>
-
           </div>
-
         )
     }
 }
