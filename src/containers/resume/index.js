f5324eb069c36ca65dab5cb282425b318f830362
Manhart, John <manhartjohn@gmail.com>
2018-03-04 22:06:43 -0600

    TImeline is looking siiick

--- src/containers/resume/index.js
+++ src/containers/resume/index.js
@@ -6,8 +6,11 @@ import { Link } from 'react-router';
 
 // import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-//Import Local Styles
+//Import Global Styles
 import '../../main.scss'
+
+//Import Local Styles
+import './styles.scss'
 
 //Import Components
 import BackIcon from '../../components/backIcon'
@@ -32,14 +35,6 @@ export default class Resume extends Component{
               A person that oversees product vision from a high level (how does this feature make sense for where we want to be in 6 months) to a low execution level (how does styling this button this way impact how the user flows through this flow).
             </span>
           </div>
-          <div className="block definition">
-            <span className="label-03 body-color definition-number">
-              2.
-            </span>
-            <span className="label-03 body-color">
-              A problem solver.
-            </span>
-          </div>
         </div>
       )
     }
@@ -47,26 +42,60 @@ export default class Resume extends Component{
     renderExpandedDefintion(){
       return(
         <div className="block">
-            <span className="label-03 body-color">
-              <hr/>
-                Skills<br/>
-                - HTML <br/>
-                - CSS <br/>
-                  -- Preprocessors <br/>
-                  -- SCSS<br/>
-                  -- Less<br/>
-                - JavaScript  <br/>
-                  -- ES6<br/>
-                  -- React<br/>
-                  -- React Native<br/>
-                  -- Package Manager <br/>
-                  -- npm<br/>
-                  -- Yarn<br/>
-                - Swift<br/>
-                - Python<br/>
-                - Git<br/>
-            </span>
+          <span className="label-03 body-color">
+          <ul className="timeline">
 
+            <li>
+              <div className="timeline-entry">
+                <span className="timeline-label">Garmin UX Designer</span>
+                <span className="timeline-date">2017-present</span>
+                <span>Lorem ipsum dolor amet squid fam 8-bit, wayfarers dreamcatcher artisan marfa poke seitan gastropub vice butcher ugh meditation. Glossier cloud bread scenester brunch, church-key prism forage raw denim affogato retro. Hammock put a bird on it vegan tattooed.</span>
+              </div>
+            </li>
+
+            <li>
+              <div className="timeline-entry">
+                <span className="timeline-label">UX Designer</span>
+                <span className="timeline-position">Garmin</span>
+                <span className="timeline-date">2017-present</span>
+              </div>
+            </li>
+
+            <li>
+              <div className="timeline-entry">
+                <span className="timeline-label">UX Designer</span>
+                <span className="timeline-position">Garmin</span>
+                <span className="timeline-date">2017-present</span>
+              </div>
+            </li>
+
+            <li>
+              <div className="timeline-entry">
+                <span className="timeline-label">UX Designer</span>
+                <span className="timeline-position">Garmin</span>
+                <span className="timeline-date">2017-present</span>
+              </div>
+            </li>
+
+            <li>
+              <div className="timeline-entry">
+                <span className="timeline-label">UX Designer</span>
+                <span className="timeline-position">Garmin</span>
+                <span className="timeline-date">2017-present</span>
+              </div>
+            </li>
+
+            <li>
+              <div className="timeline-entry">
+                <span className="timeline-label">UX Designer</span>
+                <span className="timeline-position">Garmin</span>
+                <span className="timeline-date">2017-present</span>
+              </div>
+            </li>
+
+
+          </ul>
+          </span>
         </div>
       )
     }
@@ -86,3 +115,19 @@ export default class Resume extends Component{
         )
     }
 }
+
+{/*
+  <ul>
+    <li>Garmin UX Designer
+      <p>2017 – Present</p>
+      <ul>
+        <li>second item first subitem</li>
+        <li>second item second subitem
+        </li>
+        <li>second item third subitem</li>
+      </ul>
+    </li>
+    <li>Marlin</li>
+    <li>Missouri State</li>
+  </ul>
+*/}
