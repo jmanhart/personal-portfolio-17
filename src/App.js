78022d08d986395e512266ae8bf54c6ab154f2d2
Manhart, John <manhartjohn@gmail.com>
2018-03-05 22:03:48 -0600

    cleaning up the app.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import ReactDOM from 'react-dom';
 import Interactive from 'react-interactive';
 import { Link } from 'react-router';
 
-//Import Components
-import Footer from './components/footer'
-import MenuNav from './components/menuNav'
-import MenuIcon from './components/menuIcon'
-import MobileNav from './components/mobileNav'
-
 //Import Styles
 import './main.scss'
 import './lib/vars.scss';
@@ -20,59 +14,16 @@ const propTypes = {
   routes: PropTypes.array.isRequired,
 };
 
-let barColor = '#ff9d18'
 
 export default class App extends Component{
-    constructor(){
-        super()
-        this.state ={
-            nav: false
-        }
-        this.toggleNav = this.toggleNav.bind(this)
-        this.closeNav = this.closeNav.bind(this)
-    }
 
-    toggleNav(){
-        console.log('Working thug')
-        this.setState({
-            nav: !this.state.toggleNav
-        })
-    }
-
-    closeNav(){
-        console.log('Yo, its closed')
-        this.setState({
-            nav: false
-        })
-    }
-
-    getColor() {
-      console.log("hi")
-        let color;
-        switch(window.location.pathname) {
-            case '/': color = "blue";
-            case '/blog': color= "green";
-        }
-    }
-
-    render(color){
+    render(){
         return(
-            <div className="wrapper" className={(this.state.nav) ? "nav--open" : null}>
-                {this.getColor()}
-
-      
-                <MenuNav closeNav={this.closeNav}/>
-                    <div id="content">
-                        {this.props.children}
-                    </div>
-                {/* <MobileNav />
-                  <div className="left" style={{backgroundColor: barColor}}></div>
-                  <div className="right" style={{backgroundColor: barColor}}></div>
-                  <div className="top" style={{backgroundColor: barColor}}></div>
-                  <Footer />*/}
-
-
+          <div className="wrapper">
+            <div id="content">
+                {this.props.children}
             </div>
+          </div>
         )
     }
 }
