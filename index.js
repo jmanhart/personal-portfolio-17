5c9d2051b545c365fab8159c49e301b76c7690d9
Manhart, John <manhartjohn@gmail.com>
2018-03-05 22:08:54 -0600

    maybe?

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var app = express()
+
+var PORT = process.env.PORT || 8081
+
+app.use(express.static(__dirname + '/dist'))
+
+// Required for server to resolve routes other than /
+app.get('*', function (req, res){
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(PORT, function() {
+	console.log('Server listening on port ', PORT)
+})
