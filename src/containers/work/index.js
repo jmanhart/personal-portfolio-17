3f750b018d8bace9c0368fe52cb0e209c78c86e8
Manhart, John <manhartjohn@gmail.com>
2018-07-16 20:23:19 -0500

    Pushing up work to the work page. Need to create a hero case study card. Can leave the meetups and sketchbooks in the list format.

--- src/containers/work/index.js
+++ src/containers/work/index.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router';
 // import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 //Import Styles
-// import './styles.scss'
+import './styles.scss'
 // import '../../main.scss'
 
 //Import Components
@@ -32,38 +32,159 @@ const data = [
       "cardLocation": "View on Project",
       "cardLink":"/cobb-case-study"
     },
-
 ]
-
+const dataMeetups = [
+    {
+      "cardTitle": "Framer Kansas City",
+      "cardSubText": "Mobile application for internal use.",
+      "cardImage":"./src/public/images/logos/framer.png",
+      "cardLocation": "View on Facebook group page",
+      "cardLink":"/mosey-case-study"
+    },
+    {
+      "cardLink": "https://www.meetup.com/Sketch-Design-Kansas-City/",
+      "cardTitle": "K.C. Sketch Meetup",
+      "cardSubText": "Orginazier of the official Sketch Meetup for Kansas City.",
+      "cardImage":"./src/public/images/logos/sketch-kc.png",
+      "cardLocation": "View on Meetup",
+      "link":"/coffee/kansas-city"
+    },
+]
+const dataCodeSketchbooks = [
+  {
+    "cardTitle": "Framer Sketchbook",
+    "cardSubText": "Collection of Framer basics, sketchs, and designs.",
+    "cardImage":"./src/public/images/logos/framer.png",
+    "cardLocation": "View on Github",
+    "cardLink":"https://github.com/jmanhart/framer-sketchbook"
+  },
+  {
+    "cardTitle": "Swift Sketchbook",
+    "cardSubText": "Sketchs and tests using Swift to build UI nativly for iOS.",
+    "cardImage":"./src/public/images/logos/swift.png",
+    "cardLocation": "View on Github",
+    "cardLink":"https://github.com/jmanhart/swift-sketchbook"
+  },
+  {
+    "cardTitle": "Sketch Plugin Sketchbook",
+    "cardSubText": "Sketchs and tests using Swift to build UI nativly for iOS.",
+    "cardImage":"./src/public/images/logos/sketch.png",
+    "cardLocation": "View on Github",
+    "cardLink":"https://github.com/jmanhart/react-sketchapp"
+  },
+  {
+    "cardTitle": "React-SketchApp",
+    "cardSubText": "Sketchs and tests using Swift to build UI nativly for iOS.",
+    "cardImage":"./src/public/images/logos/react-sketchapp.png",
+    "cardLocation": "View on Github",
+    "cardLink":"https://github.com/jmanhart/react-sketchapp"
+  },
+  {
+    "cardTitle": "Python Sketchbook",
+    "cardSubText": "Sketchs and tests using Swift to build UI nativly for iOS.",
+    "cardImage":"./src/public/images/logos/python.png",
+    "cardLocation": "View on Github",
+    "cardLink":"https://github.com/jmanhart/react-sketchapp"
+  },
+]
 export default class Work extends Component{
-    renderHeroDefinition(){
+    renderIntroduction(){
       return(
         <div>
           <div className="block flex-colum spacer-below">
-            <span className="label-01">Recent Work</span>
+            <span className="label-01">Work</span>
             <span className="label-03 body-color">
-              Collection of relavent work, projects, and community engagement.
+              Collection of case studies, code sketchbooks, and community outreach within Kansas City. Also anything else that makes the cut!
             </span>
           </div>
         </div>
       )
     }
+    renderCasesStudies(){
+      return(
+        <div className="blockIII">
+          <div className="section-header">
+            <span className="label-04 body-color">
+              Case Studies
+            </span>
+            <span className="label-04 section-count">
+              {data.length}
+            </span>
+          </div>
+          {data.map((item) => {
+            return (
+              <WorkCard
+                cardLink={item.cardLink}
+                cardTitle={item.cardTitle}
+                cardSubText={item.cardSubText}
+                cardImage={item.cardImage}
+                cardLocation={item.cardLocation}
+                />
+            )
+          })}
+        </div>
+      )
+    }
+    renderCodeSketchbooks(){
+      return(
+        <div className="blockIII">
+          <div className="section-header">
+            <span className="label-04 body-color">
+              Code Sketchbooks
+            </span>
+            <span className="label-04 section-count">
+              {dataCodeSketchbooks.length}
+            </span>
+          </div>
+          {dataCodeSketchbooks.map((item) => {
+            return (
+              <WorkCard
+                cardLink={item.cardLink}
+                cardTitle={item.cardTitle}
+                cardSubText={item.cardSubText}
+                cardImage={item.cardImage}
+                cardLocation={item.cardLocation}
+                />
+            )
+          })}
+        </div>
+      )
+    }
+
+    renderMeetups(){
+      return(
+        <div className="blockIII">
+          <div className="section-header">
+            <span className="label-04 body-color">
+              User Groups
+            </span>
+            <span className="label-04 section-count">
+              {dataMeetups.length}
+            </span>
+          </div>
+          {dataMeetups.map((item) => {
+            return (
+              <WorkCard
+                cardLink={item.cardLink}
+                cardTitle={item.cardTitle}
+                cardSubText={item.cardSubText}
+                cardImage={item.cardImage}
+                cardLocation={item.cardLocation}
+                />
+            )
+          })}
+        </div>
+      )
+    }
+
     render(){
         return(
             <div className="wrapper">
               <BackIcon/>
-              {this.renderHeroDefinition()}
-              {data.map((item) => {
-                return (
-                  <WorkCard
-                    cardLink={item.cardLink}
-                    cardTitle={item.cardTitle}
-                    cardSubText={item.cardSubText}
-                    cardImage={item.cardImage}
-                    cardLocation={item.cardLocation}
-                    />
-                )
-              })}
+              {this.renderIntroduction()}
+              {this.renderCasesStudies()}
+              {this.renderMeetups()}
+              {this.renderCodeSketchbooks()}
             </div>
         )
     }
@@ -77,8 +198,8 @@ export default class Work extends Component{
     "cardImage":"./src/public/images/logos/sketch-kc.png",
     "cardLocation": "View on Meetup",
     "link":"/coffee/kansas-city"
-  }, 
-  
+  },
+
   {
     "cardTitle": "Framer Sketchbook",
     "cardSubText": "Collection of Framer basics, sketchs, and designs.",
