7d61afb2fb93e0659d416a389d237ec2791e92fc
Manhart, John <manhartjohn@gmail.com>
2018-09-30 13:42:41 -0500

    further style guide work!

--- src/containers/colophon/index.js
+++ src/containers/colophon/index.js
@@ -13,7 +13,7 @@ import './styles.scss'
 import BackIcon from '../../components/backIcon'
 
 
-export default class Colophon extends Component{
+export default class Colophon extends React.Component{
     renderHeroDefinition(){
       return(
         <div>
@@ -31,68 +31,107 @@ export default class Colophon extends Component{
       )
     }
 
+
+
+
+
+
+
+    renderColors(){
+      return(
+        <div>
+          
+          <div className="flex-column basic-row">
+            <code className="spacer-bottom-small">Colors</code>
+            
+            <div className="flex-row basic-row">
+              
+              <div className="color-swatch" style={{backgroundColor:'#000'}}>
+                <code className="label">#000</code>
+              </div>
+              
+              <div className="color-swatch" style={{backgroundColor:'#222'}}>
+                <code className="label">#222</code>
+              </div>
+              
+              <div className="color-swatch" style={{backgroundColor:'#444'}}>
+                <code className="label">#444</code>
+              </div>
+              
+              <div className="color-swatch" style={{backgroundColor:'#888'}}>
+                <code className="label">#888</code>
+              </div>
+              
+              <div className="color-swatch" style={{backgroundColor:'#e5e5e5', border:'1px solid #e5e5e5'}}>
+                <code className="label" style={{color:'#444'}}>#e5e5e5</code>
+              </div>
+              
+              <div className="color-swatch" style={{backgroundColor:'#fff', border:'1px solid #e5e5e5'}}>
+                <code className="label" style={{color:'#444'}}>#fff</code>
+              </div>
+              
+            </div>
+            
+          </div>
+          
+          
+        </div>
+      )
+    }
+
+
+
+
+
     renderTypeStyles(){
       return(
           <div>
             
             <div className="flex-column basic-row">
-              <span className="label-01">Hero Title</span>
-              <code>.label-01</code>
+              <code className="spacer-bottom-small">.label-01</code>
+              <span className="label-01">Super Awesome Title!</span>
+            </div>
+            
+      
+            <div className="flex-column basic-row">
+              <code className="spacer-bottom-small">.label-02</code>
+              <span className="label-02">Hero sub title for added support to hero. This is great awesome yay!</span>
             </div>
             
 
             
             <div className="flex-column basic-row">
-              <span className="label-02">Hero sub title for added support to hero.</span>
-              <code>.label-02</code>
-            </div>
-            
-
-            
-            <div className="flex-column basic-row">
+              <code className="spacer-bottom-small">.label-03</code>
               <span className="label-03">
                 Hero body copy for further lead in drop – Lorem ipsum dolor amet poke vinyl put a bird on it disrupt flexitarian helvetica polaroid craft beer, before they sold out ramps woke. XOXO helvetica prism, coloring book bicycle rights gentrify narwhal.
               </span>
-              <code>.label-03</code>
-            </div>
-            
-
-            
-            <div className="flex-column basic-row">
-              <span className="label-04">
-                Article Sub Header 
-              </span>
-              <code>.label-04</code>
-            </div>
-            
-
-            
-            <div className="flex-column basic-row">
-              <span className="label-05">
-                Caption Text
-              </span>
-              <code>.label-05</code>
             </div>
             
             
             <div className="flex-column basic-row">
-              <span className="label-06">
-                Lorem ipsum dolor amet.
-              </span>
-              <code>.label-06</code>
+              <code className="spacer-bottom-small">.roll-over</code>
+              <div>
+                <span className="roll-over">
+                  Internal Link 
+                </span>
+              </div>
             </div>
             
             <div className="flex-column basic-row">
-              <span className="label-footer">
-                Label Footer Text
-              </span>
-              <code>.label-footer</code>
+              <code className="spacer-bottom-small">.label-footer</code>
+              <div>
+                <span className="label-footer">
+                  Outside Link
+                </span>
+              </div>
             </div>
             
           </div>
     
       )
     }
+
+
 
     renderExpandedDefintion(){
       return(
@@ -115,7 +154,9 @@ export default class Colophon extends Component{
             <div className="wrapper-scroll">
               <BackIcon />
               {this.renderTypeStyles()}
+              {this.renderColors()}
             </div>
+
     
         )
     }
@@ -127,4 +168,40 @@ export default class Colophon extends Component{
       A person that oversees product vision from a high level (how does this feature make sense for where we want to be in 6 months) to a low execution level (how does styling this button this way impact how the user flows through this flow).
     </span>
   </div>  
+  
+  <div className="flex-column basic-row">
+    <span className="label-05">
+      Caption Text
+    </span>
+    <code>.label-05</code>
+  </div>
+  
+  
+  <div className="flex-column basic-row">
+    <span className="label-06">
+      Lorem ipsum dolor amet.
+    </span>
+    <code>.label-06</code>
+  </div>
+  
+  <div className="flex-column basic-row">
+    <span className="label-footer">
+      Label Footer Text
+    </span>
+    <code>.label-footer</code>
+  </div>
+  
+  <div className="flex-column basic-row">
+    <span className="label-04">
+      Article Sub Header 
+    </span>
+    <code>.label-04</code>
+  </div>
+  
+  
+  <span className="label-05 spacer-bottom-medium">Usage:</span>
+  <span className="label-03">
+    Hero body copy for further lead in drop – Lorem ipsum dolor amet poke vinyl put a bird on it disrupt flexitarian helvetica polaroid craft beer, before they sold out ramps woke. XOXO helvetica prism, coloring book bicycle rights gentrify narwhal.
+  </span>
+  
 */}
